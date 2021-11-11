44bfa049d55b1c7082aa325d5b4ff1e78ebc5772
Nabokov Nikolai <n.nabokov1109@gmail.com>
2021-11-11 20:22:54 +0300

    remade player

--- Gravity Dash!/Assets/Scripts/Game.cs
+++ Gravity Dash!/Assets/Scripts/Game.cs
@@ -5,32 +5,23 @@ using Random = UnityEngine.Random;
 
 public class Game : MonoBehaviour
 {
-    public static bool buttonActive;
-
     private static GameObject bonus;
     private static GameObject player;
 
     private static int Score;
     [SerializeField] private Text ScoreText;
     
-    public void Start()
+    public void Awake()
     {
         Score = 0;
-        buttonActive = false;
         
-        // переменные статыческие => нужно их задать так, а не через [SerializeField]
+        // переменные статические => нужно их задать так, а не через [SerializeField]
         bonus = GameObject.Find("Main Camera/Canvas/Game Field/Bonus");
         player = GameObject.Find("Main Camera/Canvas/Game Field/Player");
         
         // установка обьектов на старт позиции
         bonus.transform.localPosition = new Vector3(Random.Range(-.3f, .3f), .425f, 0);
         player.transform.localPosition = new Vector3(0,0,0);
-        
-    }
-
-    public void OnClick()
-    {
-        buttonActive = !buttonActive;
     }
 
     private void FixedUpdate()
--- Gravity Dash!/Assets/Scripts/Player.cs
+++ Gravity Dash!/Assets/Scripts/Player.cs
@@ -1,13 +1,28 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private GameObject _bonus;
     [SerializeField] private GameObject _player;
-    private Vector3 direction;
-    [SerializeField] private float speed;
+    [SerializeField] private GameObject _playerBody;
 
+    private Vector3 direction;
+    [SerializeField] public float playerSpeed;
+    [SerializeField] public float playerRotationSpeed;
+
+    public void OnClick() {playerSpeed = -playerSpeed;}
+
+    private void ChangePlayerDirection()
+    {
+        // вычисление направления движения игрока
+        var bonusPosition = _bonus.transform.localPosition;
+        var playerPosition = _player.transform.localPosition;
+        direction = new Vector2(bonusPosition.x - playerPosition.x, bonusPosition.y - playerPosition.y).normalized;
+    }
     
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -20,20 +35,20 @@ public class Player : MonoBehaviour
                 Dead();
                 break;
         }
+        
+        ChangePlayerDirection();
+    }
+
+    private void Start()
+    {
+        ChangePlayerDirection();
     }
 
     private void FixedUpdate()
     {
-        // вычисление направления движения игрока
-        var bonusPosition = _bonus.transform.localPosition;
-        var playerPosition = _player.transform.localPosition;
-        direction = new Vector2(bonusPosition.x - playerPosition.x, bonusPosition.y - playerPosition.y).normalized;
-        
         // движение игрока
-        if (!Game.buttonActive)
-            _player.transform.Translate(direction * speed);
-        else
-            _player.transform.Translate(direction * -speed);
+        _player.transform.Translate(direction * playerSpeed);
+        _playerBody.transform.Rotate(0, 0, playerRotationSpeed);
     }
 
     private void Dead()
